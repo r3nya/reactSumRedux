e9058d0fae3151c2a8026dc5c69e111f6be72b1f
Federico Squartini <federico.squartini@gmail.com>
2015-11-30 10:50:06 +0100

    Update to ES6

--- reactSum.jsx
+++ reactSum.jsx
@@ -1,47 +1,40 @@
-var React = require('react');
+const React = require('react');
 // Provides render
-var ReactDom = require('react-dom');
-var Redux = require('redux');
+const ReactDom = require('react-dom');
+const Redux = require('redux');
 // Provides Immutable Map
-var Immutable = require('immutable');
+const Immutable = require('immutable');
 // Provides connect() and <Provide>
-var ReactRedux =  require('react-redux');
+const ReactRedux =  require('react-redux');
 
 // ACTIONS
-function sumX (n) {
- return {type: 'x', value: parseInt(n)};
-}
+const sumX = n => ({ type: 'x', value: parseInt(n)});
 
-function sumY (n) {
- return {type: 'y', value: parseInt(n)};
-}
+const sumY = n => ({ type: 'y', value: parseInt(n)});
 
 // REDUCER
-function adder (state = Immutable.Map({ x: 0, y: 0, z: 0}), action) {
+const adder = (state = Immutable.Map({ x: 0, y: 0, z: 0}), action) => {
     switch (action.type) {
         case 'x': return state.set('x',action.value)
-                              .set('z',action.value + state.get('y'));
-                  break;
+            .set('z',action.value + state.get('y'));
+            break;
         case 'y': return state.set('y',action.value)
-                              .set('z',state.get('x') + action.value);
-                  break;
+            .set('z',state.get('x') + action.value);
+            break;
     }
-}
-
-// STORE
-var store = Redux.createStore(adder);
+};
 
 // COMPONENTS
-var Numbox1 = React.createClass ({
+const Numbox1 = React.createClass ({
     render: function() {
         return ( <input type="number"
-                  value={this.props.value}
-                  onChange={e => dispatch(sumX(e.target.value))}/>
+                value={this.props.value}
+                onChange={e => dispatch(sumX(e.target.value))}/>
                );
     }
 });
 
-var Numbox2 = React.createClass ({
+const Numbox2 = React.createClass ({
     render: function() {
         return (<input type="number"
                 value={this.props.value}
@@ -50,20 +43,22 @@ var Numbox2 = React.createClass ({
     }
 });
 
-var Output = React.createClass ({
+const Output = React.createClass ({
     render: function () {
         return <p> {this.props.result} </p>;
     }
 });
 
-var Adder = React.createClass ({
+const Adder = React.createClass ({
     render: function () {
+        console.log("CONTEXT");
+        console.log(this.context.keys);
         console.log(this.props);
         const { dispatch, s } = this.props;
         return (<div>
-                 <Numbox1 value={s.x}/>
-                 <Numbox2 value={s.y}/>
-                 <Output result={s.z}/>
+                <Numbox1 value={s.x}/>
+                <Numbox2 value={s.y}/>
+                <Output result={s.z}/>
                 </div>);
     }
 });
@@ -72,8 +67,8 @@ var Adder = React.createClass ({
 ReactRedux.connect(s => s)(Adder);
 
 ReactDom.render(
-     <ReactRedux.Provider store={store}>
-         <Adder/>
-     </ReactRedux.Provider>,
-     document.getElementById('app'));
+    <ReactRedux.Provider store={Redux.createStore(adder)}>
+    <Adder/>
+    </ReactRedux.Provider>,
+    document.getElementById('app'));
 
