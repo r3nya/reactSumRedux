e281e5f666f44371003879ab168173ccc2c773f1
Federico Squartini <federico.squartini@gmail.com>
2015-12-01 11:59:41 +0100

    More ES6

--- reactSum.jsx
+++ reactSum.jsx
@@ -13,36 +13,45 @@ const sumX = n => ({ type: 'x', value: parseInt(n)});
 const sumY = n => ({ type: 'y', value: parseInt(n)});
 
 // REDUCER
-const adder = (state = Immutable.Map({ x: 0, y: 0, z: 0}), action) => {
-    switch (action.type) {
-        case 'x': return state.set('x',action.value)
-            .set('z',action.value + state.get('y'));
-        case 'y': return state.set('y',action.value)
-            .set('z',state.get('x') + action.value);
+const adder = (state = Immutable.Map({ x: 0, y: 0, z: 0}), {value, type}) => {
+    switch (type) {
+        case 'x': return state.set('x', value)
+            .set('z', value + state.get('y'));
+        case 'y': return state.set('y', value)
+            .set('z', state.get('x') + value);
         default: return state;
     }
 };
 
 // COMPONENTS
-const Numbox1 = (props, context) => ( <input type="number"
-                          value={props.value}
-                          onChange={e => context.store.dispatch(sumX(e.target.value))}/>
-                         );
+const Numbox1 = ({value}, {store}) => {
+    console.log("value is:");
+    console.log(props);
+
+    return ( <input type="number"
+            value={value}
+            onChange={e => {
+                console.log("event value is:");
+                console.log(e.target.value);
+                return store.dispatch(sumX(e.target.value));}}/>
+           );
+};
 
 Numbox1.contextTypes = {
     store: React.PropTypes.object
 };
 
-const Numbox2 = (props, context) => ( <input type="number"
-                          value={props.value}
-                          onChange={e => context.store.dispatch(sumY(e.target.value))}/>
-                         );
+const Numbox2 = ({value}, {store}) => (
+    <input type="number"
+           value={value}
+           onChange={e => store.dispatch(sumY(e.target.value))}/>
+);
 
 Numbox2.contextTypes = {
     store: React.PropTypes.object
 };
 
-const Output = props => ( <p> {props.result} </p>);
+const Output = ({result}) => (<p> {result} </p>);
 /*
 let Adder = React.createClass ({
     render: function () {
@@ -62,13 +71,12 @@ let Adder = React.createClass ({
 });
 */
 
-const Adder = (props, context) => {
-        const { getState } = context.store;
-        const s = getState();
+const Adder = (props, {store}) => {
+        const state = store.getState();
         return (<div>
-                  <Numbox1 value={s.x}/>
-                  <Numbox2 value={s.y}/>
-                  <Output result={s.z}/>
+                  <Numbox1 value={state.x}/>
+                  <Numbox2 value={state.y}/>
+                  <Output result={state.z}/>
                 </div>);
 };
 
@@ -79,8 +87,9 @@ Adder.contextTypes = {
 // Connect the adder to the store
 ReactRedux.connect(s => s)(Adder);
 
-ReactDom.render(<ReactRedux.Provider store={Redux.createStore(adder)}>
-                <Adder/>
-                </ReactRedux.Provider>,
+ReactDom.render(
+    <ReactRedux.Provider store={Redux.createStore(adder)}>
+        <Adder/>
+    </ReactRedux.Provider>,
     document.getElementById('app'));
 
