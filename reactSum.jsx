dbe9477fbc83a0904b37b2b9db194f9eff69fffc
Federico Squartini <federico.squartini@gmail.com>
2015-12-01 17:09:32 +0100

    Remove redundant parentheses

--- reactSum.jsx
+++ reactSum.jsx
@@ -1,4 +1,5 @@
 const React = require('react');
+// import React from 'react'
 // Provides render
 const ReactDom = require('react-dom');
 const Redux = require('redux');
@@ -13,6 +14,7 @@ const change_x = n => ({ type: 'x', value: parseInt(n)});
 const change_y = n => ({ type: 'y', value: parseInt(n)});
 
 // REDUCER
+
 const adder = (state = Immutable.Map({ x: 0, y: 0, z: 0}), {value, type}) => {
     switch (type) {
         case 'x': return state.set('x', value)
@@ -24,24 +26,19 @@ const adder = (state = Immutable.Map({ x: 0, y: 0, z: 0}), {value, type}) => {
 };
 
 // COMPONENTS
-const Numbox = ({value, action}) => {
-    return ( <input type="number"
-            value={value}
-            onChange={action}/>
-           );
-};
+const Numbox = ({value, action}) =>
+    <input type="number" value={value} onChange={action}/>;
 
-const Output = ({result}) => (<p> {result} </p>);
+const Output = ({result}) => <p> {result} </p>;
 
-const Adder = ({state,dispatch}) => {
-        return (<div>
-                  <Numbox value={state.get('x')}
-                          action={e => dispatch(change_x(e.target.value))}/>
-                  <Numbox value={state.get('y')}
-                          action={e => dispatch(change_y(e.target.value))}/>
-                  <Output result={state.get('z')}/>
-                </div>);
-};
+const Adder = ({state,dispatch}) =>
+    <div>
+       <Numbox value={state.get('x')}
+               action={e => dispatch(change_x(e.target.value))}/>
+       <Numbox value={state.get('y')}
+               action={e => dispatch(change_y(e.target.value))}/>
+       <Output result={state.get('z')}/>
+    </div>;
 
 
 // Connect the adder to the store
@@ -49,7 +46,7 @@ const ConnectedAdder = ReactRedux.connect(state => ({state: state}))(Adder);
 
 ReactDom.render(
     <ReactRedux.Provider store={Redux.createStore(adder)}>
-        <ConnectedAdder/>
+       <ConnectedAdder/>
     </ReactRedux.Provider>,
     document.getElementById('app'));
 
